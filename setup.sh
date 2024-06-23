9bc0f7679a9b3c256952c92ad8e6db464c175b76
mb <michael.a.barone@gmail.com>
2024-06-23 17:56:36 -0400

    mkdir paths

--- setup.sh
+++ setup.sh
@@ -6,9 +6,7 @@ ln -sf "$PWD/.zshenv" "$HOME"/.zshenv
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/zsh" "$XDG_CONFIG_HOME"/zsh
 
-mkdir $XDG_CONFIG_HOME/bin
-mkdir $XDG_CONFIG_HOME/state
-mkdir $XDG_CONFIG_HOME/share
-mkdir $XDG_CONFIG_HOME/share/zsh
+mkdir "$XDG_CONFIG_HOME"/bin
+mkdir "$XDG_CONFIG_HOME"/share
+mkdir "$XDG_CONFIG_HOME"/share/zsh
 
-zsh
