0a0f31f8ab49efb60a45fc705b62553335fb2d12
mb <michael.a.barone@gmail.com>
2024-06-23 17:50:45 -0400

    create local dirs

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ ln -sf "$PWD/.zshenv" "$HOME"/.zshenv
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/zsh" "$XDG_CONFIG_HOME"/zsh
 
+mkdir $XDG_CONFIG_HOME/bin
+mkdir $XDG_CONFIG_HOME/share
+mkdir $XDG_CONFIG_HOME/state
