ed9bf6a5c669a16e883ff287e21bf951c56604fc
mb <michael.a.barone@gmail.com>
2024-06-23 17:07:04 -0400

    remove nix packaging

--- setup.sh
+++ setup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 export XDG_CONFIG_HOME="$HOME"/.config
-mkdir -p "$XDG_CONFIG_HOME"
-mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 
 ln -sf "$PWD/.zshenv" "$HOME"/.zshenv
-
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/zsh" "$XDG_CONFIG_HOME"/zsh
 
-ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
-
-# install Nix packages from config.nix
-nix-env -iA nixpkgs.myPackages
