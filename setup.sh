8886f31622373a27392380c0aebaa7bce6494e25
mb <michael.a.barone@gmail.com>
2024-06-23 17:53:44 -0400

    create zsh local share

--- setup.sh
+++ setup.sh
@@ -7,5 +7,8 @@ ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/zsh" "$XDG_CONFIG_HOME"/zsh
 
 mkdir $XDG_CONFIG_HOME/bin
-mkdir $XDG_CONFIG_HOME/share
 mkdir $XDG_CONFIG_HOME/state
+mkdir $XDG_CONFIG_HOME/share
+mkdir $XDG_CONFIG_HOME/share/zsh
+
+zsh
